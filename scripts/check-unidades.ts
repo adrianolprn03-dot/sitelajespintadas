0a0613f259ea0b8038638fe10eee10967127fd54
Desenvolvedor Lajes <dev@lajespintadas.rn.gov.br>
2026-08-07 01:22:58 -0300

    chore: remove script temporario de verificacao

--- scripts/check-unidades.ts
+++
@@ -1,8 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-const p = new PrismaClient();
-async function run() {
-    const unidades = await p.unidadeAtendimento.findMany();
-    console.log("TOTAL UNIDADES:", unidades.length);
-    console.log(JSON.stringify(unidades, null, 2));
-}
-run().finally(() => p.$disconnect());
